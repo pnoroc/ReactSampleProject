5d4b4a60dc3f1ccafd7a320a6918dbfa85eda887
Pavel <pnoroc@gmail.com>
2016-09-08 05:23:27 -0700

    added flux to deps

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Groups from "./Groups";
 import Users from "./Users";
 
 export default class Layout extends React.Component {
@@ -10,6 +11,7 @@ export default class Layout extends React.Component {
 			<div>
 				<h2>User Table</h2>
 				<Users />
+				<Groups />
 			</div>
 		)
 	}
